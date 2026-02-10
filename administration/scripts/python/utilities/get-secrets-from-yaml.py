cdef8a86518e97cca3aae9b376b5ee4d87705957
shravya <BANDARS@emea.corpdir.net>
2026-02-10 17:43:26 +0530

    change

--- administration/scripts/python/utilities/get-secrets-from-yaml.py
+++ administration/scripts/python/utilities/get-secrets-from-yaml.py
@@ -21,21 +21,17 @@ def main(service_name, manifest):
         try:
             data = yaml.load(manifest, Loader=yaml.FullLoader)
             containers = list(data["spec"]["template"]["spec"]["containers"])
-            print(containers)
             app: list = list(
                 filter(lambda filter: filter["name"] == service_name, containers)
             )
             app = dict(app[0])
-            print(app)
             envs = list(filter(lambda filter: "valueFrom" in filter.keys(), app["env"]))
-            print(envs)
             secrets = list(
                 filter(
                     lambda filter: "secretKeyRef" in filter["valueFrom"].keys(), envs
                 )
             )
             list_secret_name = list()
-            print(secrets)
             for secret in secrets:
                 list_secret_name.append(
                     to_camel_case(secret["valueFrom"]["secretKeyRef"]["key"])
@@ -46,7 +42,8 @@ def main(service_name, manifest):
         except yaml.YAMLError as exc:
             if hasattr(exc, "problem_mark"):
                 mark = exc.problem_mark
-                print(f"Error position: ({mark.line+1}, {mark.column+1})")
+                print(f"Error position: ({mark.line+1}, {mark.column+1})", file=sys.stderr)
+                sys.exit(1)
                 sys.exit(1)
 
 
