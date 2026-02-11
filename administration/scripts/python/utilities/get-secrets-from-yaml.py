aaec0680645f7aca74da76327075bd7598b0b91d
shravya <BANDARS@emea.corpdir.net>
2026-02-11 11:15:53 +0530

    changes for github actions

--- administration/scripts/python/utilities/get-secrets-from-yaml.py
+++ administration/scripts/python/utilities/get-secrets-from-yaml.py
@@ -21,29 +21,33 @@ def main(service_name, manifest):
         try:
             data = yaml.load(manifest, Loader=yaml.FullLoader)
             containers = list(data["spec"]["template"]["spec"]["containers"])
+            print(containers)
             app: list = list(
                 filter(lambda filter: filter["name"] == service_name, containers)
             )
             app = dict(app[0])
+            print(app)
             envs = list(filter(lambda filter: "valueFrom" in filter.keys(), app["env"]))
+            print(envs)
             secrets = list(
                 filter(
                     lambda filter: "secretKeyRef" in filter["valueFrom"].keys(), envs
                 )
             )
             list_secret_name = list()
+            print(secrets)
             for secret in secrets:
                 list_secret_name.append(
                     to_camel_case(secret["valueFrom"]["secretKeyRef"]["key"])
                 )
-            # Output for GitHub Actions
-            print(f'APP_SECRETS={str.join(",",list_secret_name)}')
+            print(
+                f'##vso[task.setvariable variable=APP_SECRETS]{str.join(",",list_secret_name)}'
+            )
 
         except yaml.YAMLError as exc:
             if hasattr(exc, "problem_mark"):
                 mark = exc.problem_mark
-                print(f"Error position: ({mark.line+1}, {mark.column+1})", file=sys.stderr)
-                sys.exit(1)
+                print(f"Error position: ({mark.line+1}, {mark.column+1})")
                 sys.exit(1)
 
 
